760fcf743b744e9a2a4eeea5891880fcf0a1f583
jeremiahschwab <jeremiahschwab@gmail.com>
2015-07-12 04:05:49 -0700

    Updating plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,7 @@
 plot1 <- function() {
   
+  library(base)
+  
   ## Read the data file
   raw_file <- read.table("household_power_consumption.txt", sep = ";" , header = TRUE, stringsAsFactors=FALSE)
   
@@ -7,11 +9,11 @@ plot1 <- function() {
   formatted_file <- raw_file[,-2]
   formatted_file[,1] <- paste(raw_file[,1],raw_file[,2])
   formatted_file[,1] <- as.data.frame(strptime(formatted_file[,1], "%d/%m/%Y %H:%M:%S"))
-  colnames(formatted_file)[1] <- "DateTime"
+  colnames(formatted_file)[1] <- "Date_time"
   
   
   ## Create new data frame that subsets the required dates
-  subsetted_file <- subset(formatted_file, DateTime > "2007-01-31 23:59:59" & DateTime < "2007-02-03 00:00:00")
+  subsetted_file <- subset(formatted_file, Date_time > "2007-01-31 23:59:59" & Date_time < "2007-02-03 00:00:00")
   rownames(subsetted_file) <- NULL
   subsetted_file[,2] <- as.numeric(subsetted_file[,2])
   subsetted_file[,3] <- as.numeric(subsetted_file[,3])
@@ -20,12 +22,9 @@ plot1 <- function() {
   subsetted_file[,6] <- as.numeric(subsetted_file[,6])
   subsetted_file[,7] <- as.numeric(subsetted_file[,7])
 
-  ## Create plot1
-  plot1 <- hist(subsetted_file[,2], xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col="red")
-  
   ## Create plot1.png
   png(filename="plot1.png",width=480,height=480)
-  plot(plot1, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col="red")
+  hist(subsetted_file[,2], xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col="red")
   dev.off()
   
 }
